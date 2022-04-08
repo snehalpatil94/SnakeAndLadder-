1ea3c297586b0d889be91eed63b0fe4b584dcd30
snehalpatil94 <erpatilsnehal@gmail.com>
2022-04-08 21:39:54 +0530

    Welcome to Snake And Ladder Game

---
+++ src/com/bridgelabz/snakeladder/SnakeLadder.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.snakeladder;
+
+/**
+ * Welcome Message
+ * 
+ * @author : Snehal Patil
+ *
+ */
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake And ladder Program");
+		System.out.println("Lets play");
+	}
+}
